a7823123429702ee785d54561f1b16877b36cde8
PreetiSharma15 <pwhitehat6@gmail.com>
2021-02-23 16:37:04 +0530

    model pos, rot, scale issue fix

--- js/addMarkers.js
+++ js/addMarkers.js
@@ -1,24 +1,33 @@
 AFRAME.registerComponent("create-markers", {
+  
   init: async function() {
+
     var mainScene = document.querySelector("#main-scene");
+
+    //get the dishes collection from firestore database
     var dishes = await this.getDishes();
+   
     dishes.map(dish => {
-      var marker = document.createElement("a-marker");
+      var marker = document.createElement("a-marker");   
       marker.setAttribute("id", dish.id);
       marker.setAttribute("type", "pattern");
       marker.setAttribute("url", dish.marker_pattern_url);
       marker.setAttribute("cursor", {
         rayOrigin: "mouse"
       });
+
+      //set the markerhandler component
       marker.setAttribute("markerhandler", {});
       mainScene.appendChild(marker);
 
       // Adding 3D model to scene
       var model = document.createElement("a-entity");
+    
+     
       model.setAttribute("id", `model-${dish.id}`);
-      model.setAttribute("position", dish.position);
-      model.setAttribute("rotation", dish.rotation);
-      model.setAttribute("scale", dish.scale);
+      model.setAttribute("position", dish.model_geometry.position);
+      model.setAttribute("rotation", dish.model_geometry.rotation);
+      model.setAttribute("scale", dish.model_geometry.scale);
       model.setAttribute("gltf-model", `url(${dish.model_url})`);
       model.setAttribute("gesture-handler", {});
       marker.appendChild(model);
@@ -72,6 +81,7 @@ AFRAME.registerComponent("create-markers", {
       mainPlane.appendChild(ingredients);
     });
   },
+  //function to get the dishes collection from firestore database
   getDishes: async function() {
     return await firebase
       .firestore()
--- js/markerHandler.js
+++ js/markerHandler.js
@@ -1,16 +1,22 @@
 AFRAME.registerComponent("markerhandler", {
-  init: async function() {
+  init: async function () {
+
+    //get the dishes collection from firestore database
     var dishes = await this.getDishes();
+
+    //markerFound event
     this.el.addEventListener("markerFound", () => {
-      var markerId = this.el.id;
+      var markerId = this.el.id;      
       this.handleMarkerFound(dishes, markerId);
     });
 
+    //markerLost event
     this.el.addEventListener("markerLost", () => {
       this.handleMarkerLost();
     });
+
   },
-  handleMarkerFound: function(dishes, markerId) {
+  handleMarkerFound: function (dishes, markerId) {
     // Changing button div visibility
     var buttonDiv = document.getElementById("button-div");
     buttonDiv.style.display = "flex";
@@ -18,15 +24,8 @@ AFRAME.registerComponent("markerhandler", {
     var ratingButton = document.getElementById("rating-button");
     var orderButtton = document.getElementById("order-button");
 
-    // Changing Model scale to initial scale
-    var dish = dishes.filter(dish => dish.id === markerId)[0];
-    var model = document.querySelector(`#model-${dish.id}`);
-    model.setAttribute("position", dish.model_geometry.position);
-    model.setAttribute("rotation", dish.model_geometry.rotation);
-    model.setAttribute("scale", dish.model_geometry.scale);
-
     // Handling Click Events
-    ratingButton.addEventListener("click", function() {
+    ratingButton.addEventListener("click", function () {
       swal({
         icon: "warning",
         title: "Rate Dish",
@@ -41,8 +40,23 @@ AFRAME.registerComponent("markerhandler", {
         text: "Your order will serve soon on your table!"
       });
     });
+
+    // Changing Model scale to initial scale
+    var dish = dishes.filter(dish => dish.id === markerId)[0];
+
+    var model = document.querySelector(`#model-${dish.id}`);
+    model.setAttribute("position", dish.model_geometry.position);
+    model.setAttribute("rotation", dish.model_geometry.rotation);
+    model.setAttribute("scale", dish.model_geometry.scale);
   },
-  getDishes: async function() {
+
+  handleMarkerLost: function () {
+    // Changing button div visibility
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.style.display = "none";
+  },
+  //get the dishes collection from firestore database
+  getDishes: async function () {
     return await firebase
       .firestore()
       .collection("dishes")
@@ -50,10 +64,5 @@ AFRAME.registerComponent("markerhandler", {
       .then(snap => {
         return snap.docs.map(doc => doc.data());
       });
-  },
-  handleMarkerLost: function() {
-    // Changing button div visibility
-    var buttonDiv = document.getElementById("button-div");
-    buttonDiv.style.display = "none";
   }
 });
